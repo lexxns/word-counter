4224a174606be4f230c8328a45059eaa6eb7d340
jordan.aspinall <lexxns@yahoo.co.uk>
2021-02-21 11:13:55 +0000

    Calculate + list all unique lengths of words

--- src/test/java/org/jaspinall/file/FileResourceTest.java
+++ src/test/java/org/jaspinall/file/FileResourceTest.java
@@ -11,27 +11,27 @@ import io.restassured.http.ContentType;
 
 @QuarkusTest
 public class FileResourceTest {
-//    @Test
-//    public void testRedisOperations() {
-//        // verify that we have nothing
-//        given()
-//                .accept(ContentType.JSON)
-//                .when()
-//                .get("/increments")
-//                .then()
-//                .statusCode(200)
-//                .body("size()", is(0));
-//
-//        // create a first increment key with an initial value of 0
-//        given()
-//                .contentType(ContentType.TEXT)
-//                .accept(ContentType.TEXT)
-//                .body("Hello Jordan!")
-//                .when()
-//                .post("/files")
-//                .then()
-//                .statusCode(200)
-//                .body("key", is("first-key"))
-//                .body("value", is(0));
-//    }
+    @Test
+    public void testRedisOperations() {
+        // verify that we have nothing
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/files")
+                .then()
+                .statusCode(200)
+                .body("size()", is(0));
+
+        // create a first text
+        given()
+                .contentType(ContentType.TEXT)
+                .accept(ContentType.TEXT)
+                .body("Hello Jordan!")
+                .when()
+                .post("/files")
+                .then()
+                .statusCode(200)
+                .body("key", is("first-key"))
+                .body("value", is(0));
+    }
 }
